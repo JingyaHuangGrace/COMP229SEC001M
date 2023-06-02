56e4be00bc095cdd88d5ed291595c8c73b0b79dd
Jingya Huang <jhuan180@my.centennialcollege.ca>
2023-06-02 14:03:46 -0400

    modified:   Product/server.js

--- Week4/Product/server.js
+++ Week4/Product/server.js
@@ -6,11 +6,37 @@ const cors = require("cors");
 
 const app = express();
 
+const db = require("./app/models");
+
+db.mongoose
+
+  .connect(db.url, {
+
+    useNewUrlParser: true,
+
+    useUnifiedTopology: true
+
+  })
+
+  .then(() => {
+
+    console.log("Connected to the database!");
+
+  })
+
+  .catch(err => {
+
+    console.log("Cannot connect to the database!", err);
+
+    process.exit();
+
+  });
+
  
 
 var corsOptions = {
 
-  origin: "http://localhost:8082"
+  origin: "http://localhost:8080"
 
 };
 
@@ -44,7 +70,7 @@ app.get("/", (req, res) => {
 
 // set port, listen for requests
 
-const PORT = process.env.PORT || 8082;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
 
