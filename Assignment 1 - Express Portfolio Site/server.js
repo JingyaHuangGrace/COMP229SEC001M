013369c2d485ee6c69d79eb04ffd8aa440b44a2c
Jingya Huang <jhuan180@my.centennialcollege.ca>
2023-06-07 14:51:51 -0400

    modified contact to show message after submitting

--- Assignment 1 - Express Portfolio Site/server.js
+++ Assignment 1 - Express Portfolio Site/server.js
@@ -6,21 +6,3 @@ app.listen(3008);
 module.exports = app;
 
 console.log('Server running at http://localhost:3008/');
-
-/*app.post('/submit', (req, res) => {
-  
-    // Redirect to the Home Page after a short delay
-    setTimeout(() => {
-      res.redirect('/');
-    }, 3000); // Adjust the delay (in milliseconds) as needed
-  });
-  
-  app.get('/', (req, res) => {
-    const message = "Message Sent!"; 
-    res.render('/submit', { message });
-  });
-  
-  // Start the server
-  app.listen(3001, () => {
-    console.log('Server running at http://localhost:3001/');
-  });*/
