581c94f07d7d7f69491c83fb06d2067ce675c207
Jingya Huang <jhuan180@my.centennialcollege.ca>
2023-06-23 13:00:15 -0400

    modified contact.ejs and server.js

--- Assignment 1 - Express Portfolio Site/server.js
+++ Assignment 1 - Express Portfolio Site/server.js
@@ -1,6 +1,25 @@
+
+//server.js-Jingya Huang 301221651 June 7, 2023
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/express');
 var app = express();
+
+app.post('/submit', (req, res, next) => {
+    // Perform any necessary tasks for form submission
+    // ...
+  
+    // Send a message
+    res.send('Form submitted successfully!');
+  
+    // Call next to pass control to the next middleware
+    next();
+  }, (req, res) => {
+    // This is the next middleware function
+    // It will be executed after the previous middleware completes
+    // Redirect the user to the home page
+    res.redirect('/');
+  });
+
 
 app.listen(3008);
 module.exports = app;
